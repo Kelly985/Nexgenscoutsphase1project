834ab36927529bf1718bf9cb5e72ca5a10f9bf28
Kelly985 <86683835+Kelly985@users.noreply.github.com>
2023-04-06 12:04:15 +0300

    searchbox implemented successfully

--- app.js
+++ app.js
@@ -156,3 +156,35 @@ deleteButton.addEventListener('click', deletePlayer);
 .catch(error => {
 console.error('Error updating player:', error)
 })})}
+
+// searchbox implementation
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchQuery = searchInput.value;
+  fetch(`http://localhost:3000/players?q=${searchQuery}`)
+    .then(response => response.json())
+    .then(data => {
+      const playersContainer = document.getElementById('players-container');
+      playersContainer.innerHTML = ''; // Clear existing player cards
+      data.forEach(player => {
+        const playerCard = document.createElement('div');
+        playerCard.classList.add('player-card');
+        playerCard.innerHTML = `
+          <h2>${player.name}</h2>
+          <img src="${player.image}">
+          <p>Position: ${player.position}</p>
+          <p>Age: ${player.age}</p>
+          <p>Weight: ${player.weight} kg</p>
+          <p>Height: ${player.height} cm</p>
+          <p>Nationality: ${player.nationality}</p>
+          <p>Key Attribute: ${player.key_attribute}</p>
+          <button class="edit-player-button" data-player-id="${player.id}">Edit</button>
+          <button class="deleteBtn" data-player-id="${player.id}">Delete</button>`;
+        playersContainer.appendChild(playerCard);
+      })
+    })
+});
