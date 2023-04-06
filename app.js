87b810a682c29e4c4a42f6e821145808573d8076
Kelly985 <86683835+Kelly985@users.noreply.github.com>
2023-04-06 11:38:47 +0300

    successfully fetched json data

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ let slideIndex = 0;
   slides[slideIndex - 1].classList.add ("active");
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+
+fetch('http://localhost:3000/players')
+  .then(response => response.json())
+  .then(data => {
+    const playersContainer = document.getElementById('players-container');
+    data.forEach(player => {
+      const playerCard = document.createElement('div');
+      playerCard.classList.add('player-card');
+      playerCard.innerHTML = `
+        <h2>${player.name}</h2>
+        <img src="${player.image}">
+        <p>Position: ${player.position}</p>
+        <p>Age: ${player.age}</p>
+        <p>Weight: ${player.weight} kg</p>
+        <p>Height: ${player.height} cm</p>
+        <p>Nationality: ${player.nationality}</p>
+        <p>Key Attribute: ${player.key_attribute}</p>
+        <button class="edit-player-button" data-player-id="${player.id}">Edit</button>
+        <button class="deleteBtn" data-player-id="${player.id}">Delete</button>`;
+      playersContainer.appendChild(playerCard);
+    })
+});
